1ff9aeaf3878893dda66d0501efbf4bea161d804
Siddhant Lodha <siddhant.lodha6@gmail.com>
2021-11-12 19:31:12 -0800

    home page works

--- app/views.py
+++ app/views.py
@@ -1,7 +1,19 @@
 from app import app
 from flask import request, session, jsonify, render_template
 
+discussionPosts = [{"header": " who r u", "author": "Sarthak Lodha"}]
+schoolSupplyPosts = []
+carpoolPosts = []
+tutoringPosts = []
 
-@app.route('/posts', methods=['GET'])
+@app.route('/post', methods=['GET'])
 def postpg():
     return render_template('post.html')
+
+@app.route('/signup', methods=['GET'])
+def signuppg():
+    return render_template('signup.html')
+
+@app.route('/home', methods=['GET'])
+def homepg():
+    return render_template('home.html', discussionPosts=discussionPosts)
