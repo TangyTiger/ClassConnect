e098625e9e683c9048dd93c8e3f9980c6c65e1c9
Siddhant Lodha <siddhant.lodha6@gmail.com>
2021-11-12 22:00:43 -0800

    aer

--- app/static/scripts.js
+++ app/static/scripts.js
@@ -5,9 +5,25 @@ function submit() {
   if (dropdown.value == "carpool") {
     console.log("hi");
     document.getElementById("formthing").innerHTML = `
-    <input type = "text" id = "carpoolPostTitle" name = "carpoolPostTitle" placeholder="title"><br>
-    <input type = "text" id = "carpoolPostDescription" name = "carpoolPostDescription" placeholder="title"><br>
+    <p>Title (a few words describing the carpool)</p>
+    <input type = "text" id = "carpoolPostTitle" name = "carpoolPostTitle" placeholder="Title">
+    <br>
+    <br>
+    <p>Description (longer description about the carpool)</p>
+    <textarea rows = "6" cols = "50" id = "carpoolPostDescription" name = "carpoolPostDescription" placeholder="Description"></textarea>
+    <br>
+    <br>
+    <p>Your Name</p>
+    <input type = "text" id = "carpoolPostName" name = "carpoolPostName"><br>
+    <br>
+    <p>Your Email</p>
+    <input type = "text" id = "carpoolPostEmail" name = "carpoolPostEmail">
+    <br>
+    <br>
 
+    <p>Phone Number (optional)</p>
+    <input type = "text" id = "carpoolPostPhone" name = "carpoolPostPhone">
+    `
   } else if (dropdown.value == "tutor") {
     console.log("bye");
     // Insert Tutor HTML
@@ -19,3 +35,4 @@ function submit() {
     //Insert question HTML
   }
 }
+
