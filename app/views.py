117713fc56b2bb2ae275e5b28c2dff9414b3f130
Siddhant Lodha <siddhant.lodha6@gmail.com>
2021-11-12 22:26:22 -0800

    aer

--- app/views.py
+++ app/views.py
@@ -6,18 +6,36 @@ schoolSupplyPosts = []
 carpoolPosts = []
 tutoringPosts = []
 
+
 @app.route('/post', methods=['GET'])
 def postpg():
     return render_template('post.html')
+
 
 @app.route('/signup', methods=['GET'])
 def signuppg():
     return render_template('signup.html')
 
+
 @app.route('/signin', methods=['GET'])
 def signinpg():
     return render_template('signin.html')
 
+
 @app.route('/home', methods=['GET'])
 def homepg():
     return render_template('home.html', discussionPosts=discussionPosts)
+
+
+@app.route('/submitpost')
+def submitpost():
+    form = request.args
+    post = {
+        "author": form.get('name'),
+        "description": form.get('description'),
+        "phone": form.get('phone'),
+        "email": form.get('email'),
+        "title": form.get("title"),
+        "type": form.get("type")
+    }
+    print(post)
